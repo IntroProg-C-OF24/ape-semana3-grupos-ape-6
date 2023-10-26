899638eb6acca83509686e733c514d8736d49ee5
Rafaella Palacios <repalacios5@utpl.edu.ec>
2023-10-26 17:17:22 -0500

    Ejercicio 2 Corregido

--- Ejercicio02/src/ejercicio02/Ejercicio02.java
+++ Ejercicio02/src/ejercicio02/Ejercicio02.java
@@ -1,24 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ejercicio02;
 
-/**
- *
- * @author reroes
- */
 public class Ejercicio02 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         // Resolución del problema
         int var1 = 10;
-        int var2 = 2;
-        
+        int var2 = 10;
+        double respuesta;
+        respuesta = Math.pow(var1, 2)+ var2;
+        System.out.println("La respuesta es:" + respuesta);
+        // ejercicio 2 corregido Autor Rafaella Palacios
                 
     }
     
