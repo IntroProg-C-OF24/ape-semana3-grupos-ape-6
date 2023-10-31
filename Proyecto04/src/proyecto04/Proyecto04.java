329a53cd673e467e25e8d5d7a763dea20da0632b
Rafaella Palacios <repalacios5@utpl.edu.ec>
2023-10-30 20:12:35 -0500

    Ejercicio Corregido por Rafaella Palacios

---
+++ Proyecto04/src/proyecto04/Proyecto04.java
@@ -0,0 +1,20 @@
+
+package proyecto04;
+
+
+public class Proyecto04 {
+
+   
+    public static void main(String[] args) {
+        int var1 = 20;
+        int var2 = 1;
+        int var3 = 3;
+        int var4 = 2;
+        int resultado1 = (var1/(var2 + var3))+ var4;
+        boolean resultadofinal = resultado1 >= var3;
+        System.out.println(resultadofinal);
+        //ejercicio corregido por Rafaella Palacios
+    }
+    
+    
+}
