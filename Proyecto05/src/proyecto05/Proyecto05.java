f53770736f80769ec4ee4a6226560bbb6cd90de1
Axel Stewart Roman Torres <asroman3@utpl.edu.ec>
2023-10-31 22:09:58 -0500

    corregido por Axel Roman

--- Proyecto05/src/proyecto05/Proyecto05.java
+++ Proyecto05/src/proyecto05/Proyecto05.java
@@ -1,6 +1,7 @@
 package proyecto05;
 public class Proyecto05 {
     public static void main(String[] args) {
+        //resolucion del problema
         int var1 = 2;
         int var2 = 12;
         int var3 = 1;
@@ -8,6 +9,7 @@ public class Proyecto05 {
         double respuesta;
         respuesta = (Math.pow(var1, 2) + var2 - var3)/ 3;
         System.out.println("La respuesta es igual a: " + respuesta);
+        //ejercicio 5 corregido por Axel Roman
     }
     
 }
---
+++ Proyecto09/src/proyecto09/Proyecto09.java
@@ -0,0 +1,16 @@
+package proyecto09;
+public class Proyecto09 {
+    public static void main(String[] args) {
+        //resolucion del problema
+                int var1 = 81;
+        int var2 = 25;
+        int var3 = 10;
+        int var4 = 50;
+        double var5 = (Math.sqrt(var1) + (Math.sqrt(var2) * var3));
+        boolean respuesta;
+        respuesta = var5 >= var4;
+        System.out.println("La operacion da como resultado: " + respuesta);
+        //Corregido por Axel Roman
+    }
+    
+}
