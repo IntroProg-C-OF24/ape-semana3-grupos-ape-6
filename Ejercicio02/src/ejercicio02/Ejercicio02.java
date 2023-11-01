b11bf89c147302f05d7e04610bfcec6bd80b44c1
Rafaella Palacios <repalacios5@utpl.edu.ec>
2023-10-31 23:46:56 -0500

    Ejercicio Corregido

--- Ejercicio02/src/ejercicio02/Ejercicio02.java
+++ Ejercicio02/src/ejercicio02/Ejercicio02.java
@@ -3,25 +3,14 @@ package ejercicio02;
 
 public class Ejercicio02 {
 
-    
+   
     public static void main(String[] args) {
-        // Resolución a la Problemática
-        int var1 = 1;
-        int var2 = 2;
-        int var3 = 4;
-        int var4 = 3;
-        int var5 = 5;
-        int var6 = 81;
-        System.out.println("-----Solición al problema-----");
-        //Math.sqrt me permite obtener la raíz cuadrada de un número
-        double resultado = (var1 + var2 +var3 + var4) / var5 + Math.sqrt(var6);
-       
-        System.out.println(resultado);
-        //Ejercio analiza, Corregir var6 =80 y poner Var6 =81
-                
-    }
-    
-    
-    
-    
+        // Resolución del Problema 
+        int var1 = 10;
+        int var2 = 10;
+        double respuesta;
+        respuesta = Math.pow(var1, 2) + var2;
+        System.out.println("La respuesta es:" + respuesta);
+        //Ejercicio02 corregido 
+    }   
 }
